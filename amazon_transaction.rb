29cfbf798d064bd56bdaef450bf00035867c374a
salespinosa <sal@turing.io>
2018-07-05 23:09:44 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,9 +1,14 @@
 # Code your cash register here!
 class AmazonTransaction
   attr_accessor :total
+  
   def initialize(discount = 0)
     @total = 0
     @items = []
     @discount = discount
+  end
+  
+  def add_item(item)
+    @items << item
   end
 end
